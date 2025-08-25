209ad9e49a2b89dbc80aa5c2c019169b0d2c53b2
medloutthb <141374657+medloutthb@users.noreply.github.com>
2025-08-25 13:42:12 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,31 @@ from typing import Optional
 
 from fastapi import FastAPI
 
+from fastapi import FastAPI, HTTPException
+from faker import Faker
+
 app = FastAPI()
+fake = Faker()
+
+# Predefined dict of existing users
+users = {
+    "1234567890": {"date_naissance": "1995-08-25", "genre": "male"},
+    "9876543210": {"date_naissance": "2000-01-10", "genre": "female"},
+}
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/personne/{nni}")
+def get_user_by_nni(nni: str):
+    if not (nni.isdigit() and len(nni) == 10):
+        raise HTTPException(status_code=400, detail="nni not exist")
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+    # If exists in dict → return it
+    if nni in users:
+        return {"nni": nni, **users[nni]}
+
+    # Otherwise → generate fake data
+    return {
+        "nni": nni,
+        "date_naissance": fake.date_of_birth(minimum_age=18, maximum_age=80).strftime("%Y-%m-%d"),
+        "genre": fake.random_element(elements=["male", "female"])
+    }
